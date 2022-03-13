a8a008e0d791869ff8c9a56f98ce92580a663d88
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-13 15:05:07 -0700

    project variables if else while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 /**
  * main - print the alphabet in lower case followed by a new line
- * Return : 0
+ * Return :Always 0
 */
-int main (void) 
+int main (void)
 {
 	char alph;
 
 	for(alph = 'a' ; alph <= 'z' ; alph++)
 	{
-		putchar (alph) ;
+		putchar(alph);
 	}
-	putchar ('\n') ;
-	return (0) ;
+	putchar('\n');
+	return (0);
 }
