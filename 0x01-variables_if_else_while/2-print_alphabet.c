5556a736b2e14145a4e18f4604d31719b5c5bbc0
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-13 13:57:27 -0700

    variables if else while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
+#include <stdio.h>
+
 int main (void)
 {
 	char alph;
 	for (alph ='a' ; alph<='z' ;alph++)
 	{
-		putchar(alph);
+		putchar (alph);
 	}
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
