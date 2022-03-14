2b22bb3beffb6fcd0381df7942a5ddde7db1c50c
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-14 16:13:45 -0700

    project hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdin.h>
 /**
  * main - print text using system command
  *
@@ -8,7 +7,7 @@
  */
 int main(void)
 {
-	string syt ="and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+	system('echo "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"');
 
 	system("echo syt");
 	return (1);
