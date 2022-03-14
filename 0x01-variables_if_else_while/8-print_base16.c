2cc862bdcb9b4907f2681fdca993d303ebc4fe78
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-13 17:56:50 -0700

    project variables if else while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print all the numbers of 16 base  in lowercase .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0 ; num <= 16 ; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
