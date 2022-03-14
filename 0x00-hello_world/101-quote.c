c80eecc401c1c4773a206b7af92bc05d1e0d6764
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-14 15:50:34 -0700

    project hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	system("echo 'and that piece of art is useful\" - Dora korpar, 2015-10-19\n'");
+	system("echo'and that piece of art is useful\" - Dora korpar, 2015-10-19\n'");
 	return (1);
 }
