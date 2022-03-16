f744ea19030c39cad684228b7f6baf6b020c8844
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-16 09:49:55 -0700

    functions nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,15 +4,17 @@
 /**
  * _islower - funcction that checks for lowercase characters
  *
+ * c: variables lowercase character
  * Return:0 if c is lowercase otherwise 0
  */
 int _islower(int c)
 {
 
-	if (c <= 'Z' )
+	if (c <= 'Z')
 	{
 		return (0);
-	}else
+	}
+	else
 	{
 		return (1);
 	}
