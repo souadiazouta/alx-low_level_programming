a042cc7ec7d2915ac28abfe72befc57600775d1c
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-16 07:49:38 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <main.h>
+/**
+ * function - print the alphabet in lower case followed by a new line.
+ *
+ * Return: nothing.
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+
+}
