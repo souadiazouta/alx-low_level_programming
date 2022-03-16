5a31f4c81729f9d5775c27ee9ed7ae628e334af8
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-16 15:10:52 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - function that print every minute of the day of jack bauer
+ * starting from 00:00 to 23:59
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	int min,h;
+
+	for(h = 00 ; h <= 23 ; h++)
+	{
+		for(min = 00 ; min <= 59 ; min++)
+		{
+			putchar(h);
+			putchar(':');
+			putchar(min);
+			putchar('\n');
+		}
+	}
+}
