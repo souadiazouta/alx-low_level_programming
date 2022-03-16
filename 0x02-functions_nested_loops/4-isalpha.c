dc5c140771846bea4c4310b35a158a6563ec831a
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-16 10:07:05 -0700

    functions nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  * @c: variables for alphabetic character
  * Return:1 if c is lowercase otherwise 0
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 
 	if (c <= 'Z' || c <= 'z' || (c >= 99 && c <= 122) || (c >= 65 && c <= 90))
