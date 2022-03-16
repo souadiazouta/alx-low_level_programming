96a8f7f28c42cc2bf207ccf29a6de4ceedd3d161
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-16 16:08:30 -0700

    functions nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			putchar((i * j) / 10) + '0');
+			putchar(((i * j) / 10) + '0');
 			putchar(((i * j) % 10) + '0');
 			putchar(',');
 		}
