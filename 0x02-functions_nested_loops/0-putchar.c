347852430cf104ef5aaca8105a4d55b68d15fd93
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-15 18:51:00 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print _putchar
+ *
+ * Return :0 Always
+ */
+
+int main(void)
+{
+	write(1,'_putchar',1);
+	return (0);
+}
