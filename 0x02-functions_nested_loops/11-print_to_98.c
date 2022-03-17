745ca24ba335a1c0d1e68a46e1c2c01035480d85
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-16 17:06:49 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - function that print number from n to 98 
+ *
+ * @n: number start with
+ */
+
+void print_to_98(int n)
+{
+	int j;
+
+	for (j = n ; j <= 98 ; j++)
+	{
+		if (j != 0)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		if (j == 0)
+		{
+			putchar('0');
+		}
+		else if ((j) >= 10)
+		{
+			putchar(((j) / 10) + '0');
+			putchar(((j) % 10) + '0');
+		}
+		else if ((j) < 10)
+		{
+			putchar(' ');
+			putchar(((j) % 10) + '0');
+		}
+		else if (j > -10 && j < 0)
+		{
+			putchar((-((-j) % 10)) + '0');
+		}
+		else if (j < -10)
+		{
+			putchar((-((-j) / 10)) + '0');
+			putchar((-((-j) % 10)) + '0');
+		}
+		putchar('\n');
+	}
+}
