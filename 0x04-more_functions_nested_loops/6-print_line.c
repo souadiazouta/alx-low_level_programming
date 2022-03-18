d860e5dbde60c92e64e926762eaf6cd0b3b10f47
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-18 13:58:23 -0700

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_line -print the character _ n time.
+ *  
+ *  @n: variable number of time
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	
+	for (i = 0 ; i <= n ; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
