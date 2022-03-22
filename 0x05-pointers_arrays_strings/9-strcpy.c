27218ed9e5d5ac854352a9bd07755e5b66925fe4
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-22 13:11:29 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,17 +6,14 @@
  * including the terminating null byte (\n)to the buffer pointed to by dest.
  * @dest: pointer type char.
  * @src: pointer type char.
- * Return :the pointer to dest.
+ *
+ * Return: the pointer to dest.
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	if (dest == NULL)
-	{
-		return NULL;
-	}
 
 	for (i = 0 ; i < (int)strlen(src) ; i++)
 	{
