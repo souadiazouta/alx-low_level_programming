e38896f437058783599463cb98b58564941339d2
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-03-30 18:32:16 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,16 +2,16 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string.
+ * _print_rev_recursion - function that prints a string.
  * @s: string to print.
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
 		s++;
 		_puts_recursion(s);
+		_putchar(*s);
 	}
 }
