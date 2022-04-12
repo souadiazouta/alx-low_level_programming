8c3fb788c9456619a4c2ff1dcc1db9a0c2c34f61
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-04-11 18:49:36 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,7 @@
+#include "main.h"
 #include <stdio.h>
+#include <string.h>
+
 /**
  *
  * main - check the code
@@ -8,10 +11,14 @@
 
 int main(void)
 {
+	int i = 0;
 
-    
-    printf("%s\n", __FILE__);
-
-    return (0);
+	for ( i = 0 ; i < (int)strlen(__FILE__) ; i++)
+	{
+		_putchar(__FILE__);
+	}
+	_putchar('\n');
+	
+	return (0);
 
 }
