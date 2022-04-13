22016f83c847a5dea37112708feb8e4ea7af616a
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-04-13 07:30:08 -0700

    malloc free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that returns a pointer 
+ * to a 2 dimentional array of integers.
+ *
+ * @width: width of the array.
+ * @height: height of the array.
+ *
+ * Return the array or NULL on failue.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int j, i;
+	int **a;
+
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	a = (int **)malloc(sizeof(int *) * height);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < height ; i++)
+	{
+		a[i] = (int *)malloc(sizeof(int) * width);
+		if (a[i] == NULL)
+		{
+			for (j = 0; j < i ; j++)
+				free(a[j]);
+			free(a);
+			return (NULL);
+		}
+		for (j = 0 ; j < width ; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	return (a);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - function that frees a 2 dimentional
+ * grid previously created by alloc grid.
+ *
+ * @grid: address of the two dimensional grid.
+ * @height: height of the array.
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int j;
+
+
+
+	if (grid == NULL || height <= 0)
+	{
+		return
+	}
+	for (j = 0 ; j < height ; j++)
+	{
+		free(grid[j]);
+	}
+	free(grid);
+}
