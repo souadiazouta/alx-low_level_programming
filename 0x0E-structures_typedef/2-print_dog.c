5d9874354d95fc05bc945b329c5c94155d8ff070
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-04-12 19:00:14 -0700

    structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 		{
-			printf("Name: nil\n");
+			printf("Name: (nil)\n");
 		}
 		else 
 		{
@@ -26,7 +26,7 @@ void print_dog(struct dog *d)
 
 		if (d->owner == NULL)
 		{
-			printf("Owner: nil\n");
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
