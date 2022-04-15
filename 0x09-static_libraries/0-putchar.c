0f79ec963549d2743214d75118779ea71f5dd80a
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-04-15 09:36:02 -0700

    statc libraries

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - print _putchar
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	/*write(1, "_putchar\n", sizeof("_putchar\n"));*/
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - function that checks for alphabetic characters
+ *
+ * @c: variables for alphabetic character
+ * Return:1 if c is lowercase otherwise 0
+ */
+int _isalpha(int c)
+{
+
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
