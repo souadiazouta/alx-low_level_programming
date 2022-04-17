805246fd86529e6f77db3646eda27da1decbecdc
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-04-16 18:43:08 -0700

    function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -48,7 +48,7 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("error");
+		printf("Error\n");
 		exit(100);
 	}
 	return (a / b);
@@ -64,5 +64,10 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
