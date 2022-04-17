aae66fef776c1d32d994ca5929d073720a99ac3e
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-04-16 18:51:02 -0700

    function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,11 +3,11 @@
 #include "3-calc.h"
 
 /**
- * main - function
+ * main - check the code
  * @argc: numero de arg
- * @argv: prameter
+ * @argv: the tab of argement
  *
- * Return: result of operation
+ * Return: result 0
  */
 
 int main(int argc, char *argv[])
@@ -16,16 +16,19 @@ int main(int argc, char *argv[])
 	int (*cal)(int, int);
 
 
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
 	if (argv[2] == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	cal = get_op_func(argv[2]);
 
 	if (cal == NULL)
@@ -33,8 +36,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+
 	result = cal(atoi(argv[1]), atoi(argv[3]));
+
 	printf("%d\n", result);
+
 	return (0);
 }
 
