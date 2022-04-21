cf2517a02527ed18395231a78f0ac3165dc411ba
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-04-20 20:40:31 -0700

    more malloc free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _calloc - allocates memory for an array
+ *
+ * @nmemb: number of caracteres
+ * @size: size of one case
+ *
+ * Return: allocate memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	ptr = memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+
+}
