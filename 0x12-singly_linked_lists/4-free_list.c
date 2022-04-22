fd12b1796510feb6d257d1a75cb90cf55c7fb3f4
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-04-21 20:17:20 -0700

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,15 +5,18 @@
 
 /** 
  * free_list - function that free a list
+ *
  * @head: pointer
+ *
  */
 
 void free_list(list_t *head)
 {
-	if(head == NULL)
+	if (head == NULL)
 	{
 		return;
 	}
+
 	free_list(head->next);
 	free(head->str);
 	free(head);
