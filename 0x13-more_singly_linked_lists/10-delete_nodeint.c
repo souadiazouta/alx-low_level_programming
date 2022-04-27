38e35de5056267a0dd8b583d8fc5e1e26d4ada1e
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-04-26 19:41:18 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (!head || !*head)
 		return (-1);
 
-	if (index == 0=)
+	if (index == 0)
 	{
 		*head = new->next;
 		free(new);
@@ -42,7 +42,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 
 		temp = temp->next;
-		count++:
+		count++;
 	}
 
 	return (-1);
