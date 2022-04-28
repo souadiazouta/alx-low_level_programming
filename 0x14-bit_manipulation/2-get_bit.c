a63a51fcee671a337b182b42b1e29a6103f3aec8
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-04-28 10:36:41 -0700

    bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -18,7 +18,7 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	bit = (n >> index) & 1;
 
-	if((bit == 1)) || (bit == 0))
+	if((bit == 1) || (bit == 0))
 		return (bit);
 	else
 		return (-1);
