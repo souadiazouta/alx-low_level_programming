e12dee33760a6006a133714d53aa526dfca79ea1
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-05-01 19:05:39 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ *
+ * @str: pointer string.
+ *
+ * Return: string with capitalize word.
+ */
+
+char *cap_string(char *str)
+{
+
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - function that encodes a string into 1337
+ *
+ * @str: pointer string.
+ *
+ * Return: string encoded
+ */
+
+char *leet(char *c)
+{
+	char letters[] = "aAeEoOtTlL";
+	char replace[] = "4433007711";
+	int i, j;
+
+
+	for (i = 0 ; c[i] != '\0' ; i++)
+	{
+		for (j = 0 ; letters[j] ; j++)
+		{
+			if (letters[j] == c[i] || letters[j] - 32 == c[i])
+			{
+				c[i] = replace[j];
+			}
+		}
+	}
+
+	return (c);
+}
